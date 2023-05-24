f23ea7598b77dc10c9037fde971e68279bc9b86c
neerajng <neerajnrjng@gmail.com>
2023-05-24 17:59:45 +0530

    DSA3WR

---
+++ bst-dfs-pre-order.js
@@ -0,0 +1,61 @@
+class Node{
+    constructor(value){
+        this.value=value
+        this.left=null
+        this.right=null
+    }
+}
+
+class BST{
+    constructor(){
+        this.root=null
+    }
+
+    insert(value){
+        const newNode= new Node(value)
+        if(this.root===null){
+            this.root=newNode
+            return this
+        }
+        let current=this.root
+        while(true){
+            if(value===current.value) return undefined
+            if(value<current.value){
+                if(current.left===null){
+                    current.left=newNode
+                    return this
+                }
+                current=current.left
+            }else{
+                if(current.right===null){
+                    current.right=newNode
+                    return this
+                }
+                current=current.right
+            }
+
+        }
+    }
+
+    dfsPreOrder(){
+        let data=[]
+        function traverse(node){
+            data.push(node.value)
+            if(node.left) traverse(node.left)
+            if(node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return data
+    }
+}
+
+const tree = new BST()
+
+tree.insert(10)
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+
+tree.dfsPreOrder()
